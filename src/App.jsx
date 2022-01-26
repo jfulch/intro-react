dfcfec87d3f023463a4e0f6e51f89b270f340d09
jfulch <jfulch@github.com>
2022-01-26 09:10:46 -0800

    create addStudent method

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ class App extends React.Component {
     });
   }
 
-  /*Check out this addAssignment method*/
+
   addAssignment(assignmentName) {
     this.setState({
       assignments: this.state.assignments.concat(assignmentName)
@@ -34,7 +34,11 @@ class App extends React.Component {
   }
 
   /*Write an addStudent method here*/
-
+  addStudent(studentName){
+    this.setState({
+      students: this.state.students.concat(studentName)
+    });
+  }
   addGrade(assignment, student, score) {
     let grades = this.state.grades;
     let assignmentName = assignment;
